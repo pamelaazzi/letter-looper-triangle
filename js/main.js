c3405d7d03d970bd50bc73560f42d5efe7762bb0
pamelaazzi <pamelaa.1993@hotmail.com>
2016-02-27 16:47:52 -0500

    added all codes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var loopers = prompt('Enter a letter');
+
+	for (i = 0; i < 10; i++)
+	{
+
+	for (j = 0; j < i; j++){
+	document.write(loopers);
+
+	}
+	document.write(loopers);
+	document.write('<br>');
+	}
