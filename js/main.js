db11be3d3eda126b2b4aea6288bcbdce9121ed5b
pamelaazzi <pamelaa.1993@hotmail.com>
2016-02-27 18:19:47 -0500

    added the codes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 var loopers = prompt('Enter a letter');
 
-	for (i = 0; i < 10; i++)
-	{
+for (i = 0; i < 10; i++) {
 
-	for (j = 0; j < i; j++){
-	document.write(loopers);
-
-	}
-	document.write(loopers);
-	document.write('<br>');
-	}
+  for (j = 0; j < i; j++) {
+    document.write(loopers);
+  }
+  document.write(loopers);
+  document.write('<br>');
+}
